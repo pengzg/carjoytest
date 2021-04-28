31818ddfbe2688cf1e07ca935a5985cbd6511ba5
彭宗阁 <pzgmissyou@gmail.com>
2021-04-28 21:12:40 +0800

    volatile

--- src/main/java/xyz/carjoy/thread/T_001/T.java
+++ src/main/java/xyz/carjoy/thread/T_001/T.java
@@ -1,4 +1,32 @@
 package main.java.xyz.carjoy.thread.T_001;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class T {
+    volatile int count = 0;
+    void  m(){
+        for (int i =0; i<10000; i++ ) {
+            count++;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("this is main");
+        T t = new T();
+        List<Thread> threads = new ArrayList<>();
+        for (int i=0; i<10; i++) {
+            threads.add(new Thread(t::m,"thread"+i));
+        }
+
+        threads.forEach((o)->o.start());
+        threads.forEach((o)->{
+            try {
+                o.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        System.out.println(t.count);
+    }
 }
