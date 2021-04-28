7307413591e3b587bf77944a911a48876e31a73f
彭宗阁 <pzgmissyou@gmail.com>
2021-04-28 21:48:55 +0800

    finecoarselock

--- src/main/java/xyz/carjoy/thread/c02005/FineCoarseLock.java
+++ src/main/java/xyz/carjoy/thread/c02005/FineCoarseLock.java
@@ -1,5 +1,41 @@
 package main.java.xyz.carjoy.thread.c02005;
 
+import java.util.concurrent.TimeUnit;
+
 public class FineCoarseLock {
 
+    int count = 0;
+    synchronized void m1(){
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        count++;
+
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void m2(){
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        synchronized (this){
+            count++;
+        }
+
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
