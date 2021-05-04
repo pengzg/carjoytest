5badd6c7f829bd0c9a61e6ba4a2784b6b157091b
彭宗阁 <pzgmissyou@gmail.com>
2021-05-04 10:37:10 +0800

    op

--- src/main/java/xyz/carjoy/thread/c02005/T10_TestReadWriteLock.java
+++ src/main/java/xyz/carjoy/thread/c02005/T10_TestReadWriteLock.java
@@ -2,8 +2,9 @@ package main.java.xyz.carjoy.thread.c02005;
 
 public class T10_TestReadWriteLock {
 
-    public static void main(String[] args) {
-        System.out.printf("this is good ");
 
+    public static void main(String[] args) {
+        System.out.println("out");
     }
 }
+
