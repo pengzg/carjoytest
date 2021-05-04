7d2681d69b0b8cf94ead28453e388596ec32d44d
彭宗阁 <pzgmissyou@gmail.com>
2021-05-04 21:06:34 +0800

    bubbsortname

--- src/main/java/xyz/carjoy/sort/BubbleSort.java
+++ src/main/java/xyz/carjoy/sort/BubbleSort.java
@@ -3,6 +3,8 @@ package main.java.xyz.carjoy.sort;
 public class BubbleSort {
 
     public static void main(String[] args) {
-        System.out.println("bubble sort");
+        System.out.println("bubble sort for int");
+
+
     }
 }
