99bd0238cae7daa9aec38fa0ade71cc31a91fdae
pengzg <pengzg@sqkx.net>
2021-05-13 17:26:55 +0800

    synchronousqueue

---
+++ src/main/java/xyz/carjoy/thread/T_001/TestSynchronousQueue.java
@@ -0,0 +1,22 @@
+package main.java.xyz.carjoy.thread.T_001;
+
+import java.util.concurrent.SynchronousQueue;
+
+public class TestSynchronousQueue {
+    public static void main(String[] args) throws InterruptedException {
+        SynchronousQueue<String> strs = new SynchronousQueue<>();
+
+        new Thread(()->{
+            try {
+                System.out.println(strs.take());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        },"t1").start();
+
+        strs.put("aaa");
+        strs.put("bbb");
+        strs.add("aaa");
+        System.out.println(strs.size());
+    }
+}
