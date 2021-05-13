44eae43c04e3e6888a7101f6b879c302fcec9654
pengzg <pengzg@sqkx.net>
2021-05-13 16:13:39 +0800

    blockingqueue

--- src/main/java/xyz/carjoy/thread/T_001/TestArrayBlockingQueue.java
+++ src/main/java/xyz/carjoy/thread/T_001/TestArrayBlockingQueue.java
@@ -3,15 +3,24 @@ package main.java.xyz.carjoy.thread.T_001;
 import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.TimeUnit;
 
 public class TestArrayBlockingQueue {
 
-    static BlockingDeque<String> strs = new ArrayBlockingQueue<>();
+    static BlockingDeque<String> strs = (BlockingDeque<String>) new ArrayBlockingQueue<String>(10);
 
     static Random r = new Random();
     public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < 10; i++) {
              strs.put("a"+i);
         }
+
+//        strs.put("aaa");
+//
+//        strs.add("aaaa");
+//
+//        strs.offer("aaa");
+
+        strs.offer("a", 1, TimeUnit.SECONDS);
     }
 }
