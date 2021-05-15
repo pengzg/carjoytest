ced88984a656bb257c28d774da3c70fa9314c1fa
彭宗阁 <pzgmissyou@gmail.com>
2021-05-15 16:49:13 +0800

    blockingqueue

---
+++ src/main/java/xyz/carjoy/thread/threadpool/TestBlockingQueue.java
@@ -0,0 +1,43 @@
+package main.java.xyz.carjoy.thread.threadpool;
+
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class TestBlockingQueue {
+
+    static ArrayBlockingQueue<String> q1 = new ArrayBlockingQueue(1);
+    static ArrayBlockingQueue<String> q2 = new ArrayBlockingQueue(1);
+
+    public static void main(String[] args) {
+        char[] aI = "1234567".toCharArray();
+        char[] aC = "ABCDEFG".toCharArray();
+
+        new Thread(()->{
+            for (char c : aI) {
+                System.out.println(c);
+                try {
+                    q1.put("ok");
+                    q2.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "t1").start();
+        new Thread(()->{
+            for (char c : aC) {
+
+                try {
+                    q1.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(c);
+                try {
+                    q2.put("ok");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "t2").start();
+    }
+}
