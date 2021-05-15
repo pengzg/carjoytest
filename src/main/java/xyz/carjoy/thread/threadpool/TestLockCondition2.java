024dbc63a419d9a9e92975d23530b5262fcc44ec
彭宗阁 <pzgmissyou@gmail.com>
2021-05-15 14:22:01 +0800

    lockcondition2

---
+++ src/main/java/xyz/carjoy/thread/threadpool/TestLockCondition2.java
@@ -0,0 +1,49 @@
+package main.java.xyz.carjoy.thread.threadpool;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class TestLockCondition2 {
+
+    public static void main(String[] args) {
+        char[] aI = "1234567".toCharArray();
+        char[] aC = "ABCDEFG".toCharArray();
+        Lock lock = new ReentrantLock();
+        Condition condition1 = lock.newCondition();
+        Condition condition2 = lock.newCondition();
+
+        new Thread(()->{
+            try {
+                lock.lock();
+                for (char c : aI) {
+                    System.out.println(c);
+                    condition2.signal();
+                    condition1.wait();
+                }
+                condition2.signal();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        },"t1").start();
+        new Thread(()->{
+            try {
+                lock.lock();
+                for (char c : aC) {
+                    System.out.println(c);
+                    condition1.signal();
+                    condition2.wait();
+                }
+                condition1.signal();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        },"t2").start();
+    }
+}
