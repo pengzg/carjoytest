bf5e08222ad1b15d6f9d840c24a583ef7dffac9e
彭宗阁 <pzgmissyou@gmail.com>
2021-05-16 14:58:48 +0800

    singlethreadpool

---
+++ src/main/java/xyz/carjoy/thread/threadpool/TestSingleThreadPool.java
@@ -0,0 +1,17 @@
+package main.java.xyz.carjoy.thread.threadpool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TestSingleThreadPool{
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < 5; i++) {
+            final int j = i;
+
+            service.execute(()->{
+                System.out.println(j+" "+Thread.currentThread().getName());
+            });
+        }
+    }
+}
