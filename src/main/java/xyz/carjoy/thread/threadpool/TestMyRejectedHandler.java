c7b1003cb3de58c4c8251399ddba0f289f36361a
彭宗阁 <pzgmissyou@gmail.com>
2021-05-16 17:08:13 +0800

    reghandler

---
+++ src/main/java/xyz/carjoy/thread/threadpool/TestMyRejectedHandler.java
@@ -0,0 +1,21 @@
+package main.java.xyz.carjoy.thread.threadpool;
+
+import java.util.concurrent.*;
+
+public class TestMyRejectedHandler {
+
+    public static void main(String[] args) {
+        ExecutorService service = new ThreadPoolExecutor(4,4,0, TimeUnit.SECONDS, new ArrayBlockingQueue<>(6), Executors.defaultThreadFactory(), new MyHandler());
+
+    }
+
+    static class MyHandler implements RejectedExecutionHandler {
+
+        @Override
+        public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+            if (executor.getQueue().size()<10000) {
+
+            }
+        }
+    }
+}
