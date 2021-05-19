d7944d4842dc55455f742c6e30f5b9c42688d6a2
彭宗阁 <pzgmissyou@gmail.com>
2021-05-19 22:08:25 +0800

    classloader

--- src/main/java/xyz/carjoy/jvm/TestMain.java
+++ src/main/java/xyz/carjoy/jvm/TestMain.java
@@ -4,6 +4,7 @@ public class TestMain {
     public static void main(String[] args) {
         System.out.println("main method");
         System.out.println(String.class.getClassLoader());
+        System.out.println(TestMain.class.getClassLoader());
     }
 
 }
