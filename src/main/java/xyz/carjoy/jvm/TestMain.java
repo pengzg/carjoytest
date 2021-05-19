5fe834a9aa1b45d81b3d97feb4d95bfc74517dfe
彭宗阁 <pzgmissyou@gmail.com>
2021-05-19 22:05:52 +0800

    class

--- src/main/java/xyz/carjoy/jvm/TestMain.java
+++ src/main/java/xyz/carjoy/jvm/TestMain.java
@@ -3,5 +3,7 @@ package main.java.xyz.carjoy.jvm;
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("main method");
+        System.out.println(String.class.getClassLoader());
     }
+
 }
