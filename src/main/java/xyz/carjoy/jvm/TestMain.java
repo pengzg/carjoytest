e36235b91746b770c822f19909f014a0e726cfad
pengzg <pengzg@sqkx.net>
2021-05-21 16:34:38 +0800

    c

--- src/main/java/xyz/carjoy/jvm/TestMain.java
+++ src/main/java/xyz/carjoy/jvm/TestMain.java
@@ -3,5 +3,22 @@ package main.java.xyz.carjoy.jvm;
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("main method");
+        int i = 8;
+        i = i++;
+//        i = i+1;
+//        i = ++i;
+        System.out.println(i);
+        i = i++;
+        System.out.println(i);
+        System.out.println(i);
+        System.out.println(i);
+        System.out.println(i);
+        System.out.println(i);
+        System.out.println(i);
+        System.out.println(i);
+        System.out.println(i);
+        System.out.println(i);
+        System.out.println(i);
+        System.out.println(i);
     }
 }
