5e00f05a276d9a97b9f1b8480a20c8e7363555e2
彭宗阁 <pzgmissyou@gmail.com>
2021-05-23 09:40:48 +0800

    objectsize

---
+++ src/main/java/xyz/carjoy/jvm/TestObjectSizeAgent.java
@@ -0,0 +1,17 @@
+package main.java.xyz.carjoy.jvm;
+
+import java.lang.instrument.Instrumentation;
+
+public class TestObjectSizeAgent {
+    private static Instrumentation inst;
+
+    public static void premain(String agentArgs, Instrumentation _inst){
+        inst = _inst;
+    }
+
+    public static long sizeOf (Object o) {
+        return inst.getObjectSize(0);
+    }
+
+
+}
