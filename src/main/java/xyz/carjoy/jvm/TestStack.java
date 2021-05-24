f62c9c6b877dc690768a6637a64b7bb366c7816e
彭宗阁 <pzgmissyou@gmail.com>
2021-05-24 21:37:31 +0800

    stack

--- src/main/java/xyz/carjoy/jvm/TestStack.java
+++ src/main/java/xyz/carjoy/jvm/TestStack.java
@@ -1,12 +1,13 @@
 package main.java.xyz.carjoy.jvm;
 
-public class TestMain {
+public class TestStack {
     public static void main(String[] args) {
         int i = 8;
 
-        i = i++;
-        System.out.println(i);
+        int j = 200;
+
+        int k = 2000000000;
+
 
     }
-
 }
