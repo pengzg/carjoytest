8812fd9ca23dbfda690c448c9e04bd69254c98b5
彭宗阁 <pzgmissyou@gmail.com>
2021-06-06 17:31:39 +0800

    db

--- src/main/java/xyz/carjoy/db/Test.java
+++ src/main/java/xyz/carjoy/db/Test.java
@@ -1,5 +1,18 @@
 package main.java.xyz.carjoy.db;
 
+import java.lang.reflect.Method;
+
 public class Test {
+
+    public static void main(String[] args) throws Exception{
+        Class<?> aClass = Class.forName("main.java.xyz.carjoy.db.Person");
+
+
+        System.out.println(aClass.getDeclaredFields());
+        Method[] methods = aClass.getDeclaredMethods();
+        for (Method method:methods) {
+            System.out.println(method.getName());
+        }
+    }
 
 }
