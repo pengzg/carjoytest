96d0099fbd7c1d94512a9bfd8a32e53258c3861f
彭宗阁 <pzgmissyou@gmail.com>
2021-06-06 22:15:01 +0800

    person

--- src/main/java/xyz/carjoy/db/Person.java
+++ src/main/java/xyz/carjoy/db/Person.java
@@ -5,6 +5,11 @@ public class Person {
     private String name;
     private Integer age;
 
+//    public Person(String name, Integer age){
+//        this.name = name;
+//        this.age = age;
+//    }
+
     public String getName() {
         return name;
     }
--- src/main/java/xyz/carjoy/db/Student.java
+++ src/main/java/xyz/carjoy/db/Student.java
@@ -1,8 +1,29 @@
 package main.java.xyz.carjoy.db;
 
-public class Student {
+public class Student extends Person{
 
+    public String className;
+    public String address;
 
+    public Student(){
+        super();
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     public static void main(String[] args) {
 
