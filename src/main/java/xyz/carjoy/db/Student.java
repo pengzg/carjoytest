05bcbf8ac6f9c879c4d0d7525f0f910e7b7d0cb6
彭宗阁 <pzgmissyou@gmail.com>
2021-06-06 17:17:09 +0800

    person

--- src/main/java/xyz/carjoy/db/Student.java
+++ src/main/java/xyz/carjoy/db/Student.java
@@ -1,4 +1,10 @@
 package main.java.xyz.carjoy.db;
 
 public class Student {
+
+
+
+    public static void main(String[] args) {
+
+    }
 }
---
+++ src/main/java/xyz/carjoy/db/Test.java
@@ -0,0 +1,5 @@
+package main.java.xyz.carjoy.db;
+
+public class Test {
+
+}
