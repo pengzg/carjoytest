2b8a42b99b18ba782355fd52e911259ea06086c8
彭宗阁 <pzgmissyou@gmail.com>
2021-07-15 22:47:41 +0800

    create

--- src/main/java/xyz/carjoy/activemq/Sender.java
+++ src/main/java/xyz/carjoy/activemq/Sender.java
@@ -1,7 +1,36 @@
 package main.java.xyz.carjoy.activemq;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+
 public class Sender {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+       ActiveMQConnectionFactory activeMQConnectionFactory =  new ActiveMQConnectionFactory(
+               ActiveMQConnectionFactory.DEFAULT_USER,
+               ActiveMQConnectionFactory.DEFAULT_PASSWORD,
+               "tcp://42.192.16.23:61616");
+
+        Connection connection =  activeMQConnectionFactory.createConnection();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        Queue queue = session.createQueue("test01");
+
+
+        MessageProducer producer =  session.createProducer(queue);
+
+
+        TextMessage textMessage = session.createTextMessage("hi");
+
+        producer.send(textMessage);
+
+        connection.close();
+
+
+
+
 
     }
 }
