2a65d18d33732062b1df792816aedf2edbe33448
pengzg <pengzg@sqkx.net>
2021-07-22 16:23:29 +0800

    roc

--- src/main/java/xyz/carjoy/rocketmq/rmq01/Producer.java
+++ src/main/java/xyz/carjoy/rocketmq/rmq01/Producer.java
@@ -1,19 +1,21 @@
 package xyz.carjoy.rocketmq.rmq01;
 
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 
 
 public class Producer {
     public static void main(String[] args) throws Exception{
-        DefaultMQProducer defaultMQProducer = new DefaultMQProducer("testGroup");
+        DefaultMQProducer producer = new DefaultMQProducer("testtopic");
         // 设置nameserver地址
-        defaultMQProducer.setNamesrvAddr("42.192.16.23:9876");
-        defaultMQProducer.start();
+        producer.setNamesrvAddr("42.192.16.23:9876");
+        producer.start();
        // topic消息的目的地  和message绑定
-        Message msg = new Message("testgroup001","test".getBytes());
+        Message msg = new Message("test0002","tes6666t".getBytes());
 
-        defaultMQProducer.send(msg);
+        SendResult send = producer.send(msg);
+        System.out.println(send.getSendStatus());
 
     }
 }
