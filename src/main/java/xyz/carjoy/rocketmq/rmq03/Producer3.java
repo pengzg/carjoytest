a7199edd6ac59dca82024913f37cbf00afc52f28
彭宗阁 <pzgmissyou@gmail.com>
2021-07-22 21:18:00 +0800

    sendonway

---
+++ src/main/java/xyz/carjoy/rocketmq/rmq03/Producer3.java
@@ -0,0 +1,29 @@
+package xyz.carjoy.rocketmq.rmq03;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendCallback;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+
+import java.util.ArrayList;
+
+
+public class Producer3 {
+    public static void main(String[] args) throws Exception{
+        DefaultMQProducer producer = new DefaultMQProducer("testtopic");
+        // 设置nameserver地址
+        producer.setNamesrvAddr("42.192.16.23:9876");
+        producer.start();
+       // topic消息的目的地  和message绑定
+        // 单条发送
+//        Message msg = new Message("test0002","tes6666t".getBytes());
+//        SendResult send = producer.send(msg);
+        // 多条发送
+        Message msg1 = new Message("test0004","test 第一条".getBytes());
+
+
+        producer.sendOneway(msg1);
+
+        System.out.println("停止");
+    }
+}
