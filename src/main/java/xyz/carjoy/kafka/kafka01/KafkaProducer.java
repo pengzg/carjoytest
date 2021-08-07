66a54267f1bc9ff2677b9ebf0b71a8005aed7ea1
彭宗阁 <pzgmissyou@gmail.com>
2021-08-07 15:01:01 +0800

    kafka test

---
+++ src/main/java/xyz/carjoy/kafka/kafka01/KafkaProducer.java
@@ -0,0 +1,8 @@
+package xyz.carjoy.kafka.kafka01;
+
+public class KafkaProducer {
+    public static void main(String[] args) {
+        System.out.println("kafkatest" +
+                "");
+    }
+}
