8264e7a451723077fed025fe049917ecea417ff5
pengzg <pengzg@sqkx.net>
2021-08-10 11:11:29 +0800

    serilizer

--- src/main/java/xyz/carjoy/kafka/kafka07/KafkaSerializeTest.java
+++ src/main/java/xyz/carjoy/kafka/kafka07/KafkaSerializeTest.java
@@ -1,7 +1,31 @@
 package xyz.carjoy.kafka.kafka07;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.Future;
+
 public class KafkaSerializeTest {
     public static void main(String[] args) {
-        
+        // 创建kafka producer
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"CentOSA:9092,CentOSB:9092,CentOSC:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,User.class.getName());
+        KafkaProducer<String, User> kafkaProducer = new KafkaProducer<String, User>(props);
+        for (int i = 0; i < 30; i++) {
+            ProducerRecord<String, User> record = new ProducerRecord<String, String>("topic05", "key" + i, new User(i,"xiao"+i,20+i));
+            Future<RecordMetadata> send = kafkaProducer.send(record);
+            System.out.println(send.toString());
+
+
+
+        }
+        kafkaProducer.close();
+
     }
 }
--- src/main/java/xyz/carjoy/kafka/kafka07/UserDefineDeserializer.java
+++ src/main/java/xyz/carjoy/kafka/kafka07/UserDefineDeserializer.java
@@ -1,5 +1,6 @@
 package xyz.carjoy.kafka.kafka07;
 
+import org.apache.commons.lang3.SerializationUtils;
 import org.apache.kafka.common.header.Headers;
 import org.apache.kafka.common.serialization.Deserializer;
 
@@ -8,21 +9,23 @@ import java.util.Map;
 public class UserDefineDeserializer implements Deserializer {
     @Override
     public void configure(Map configs, boolean isKey) {
+        System.out.println("configure");
         Deserializer.super.configure(configs, isKey);
     }
 
     @Override
-    public Object deserialize(String s, byte[] bytes) {
-        return null;
+    public Object deserialize(String topic, byte[] data) {
+        return SerializationUtils.deserialize(data);
     }
 
     @Override
     public Object deserialize(String topic, Headers headers, byte[] data) {
-        return Deserializer.super.deserialize(topic, headers, data);
+        return SerializationUtils.deserialize(data);
     }
 
     @Override
     public void close() {
+        System.out.println("close");
         Deserializer.super.close();
     }
 }
