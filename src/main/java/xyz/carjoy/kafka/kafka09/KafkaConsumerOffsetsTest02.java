b4fb4910782dc1591414658cd94e471559c22cff
pengzg <pengzg@sqkx.net>
2021-08-10 16:39:34 +0800

    ea

--- src/main/java/xyz/carjoy/kafka/kafka09/KafkaConsumerOffsetsTest02.java
+++ src/main/java/xyz/carjoy/kafka/kafka09/KafkaConsumerOffsetsTest02.java
@@ -18,7 +18,7 @@ public class KafkaConsumerOffsetsTest02 {
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.GROUP_ID_CONFIG,"g2");
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earlest");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
 //        props.put(ConsumerConfig.INTERCEPTOR_CLASSES_CONFIG, UserDefineConsumerInterceptors.class.getName());
         KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(props);
         kafkaConsumer.subscribe(Pattern.compile("topic09"));
