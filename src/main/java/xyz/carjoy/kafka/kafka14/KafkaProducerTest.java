dadac44905d8a0540d4ba40ac7bc6900565c72a6
彭宗阁 <pzgmissyou@gmail.com>
2021-08-12 16:49:03 +0800

    k14

---
+++ src/main/java/xyz/carjoy/kafka/kafka14/KafkaProducerTest.java
@@ -0,0 +1,30 @@
+package xyz.carjoy.kafka.kafka14;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.Future;
+
+public class KafkaProducerTest {
+    public static void main(String[] args) {
+        // 创建kafka producer
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"CentOSA:9092,CentOSB:9092,CentOSC:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String, String>(props);
+        for (int i = 0; i < 30; i++) {
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>("topic05", "key" + i, "val" + i);
+            Future<RecordMetadata> send = kafkaProducer.send(record);
+            System.out.println(send.toString());
+
+
+
+        }
+        kafkaProducer.close();
+    }
+}
