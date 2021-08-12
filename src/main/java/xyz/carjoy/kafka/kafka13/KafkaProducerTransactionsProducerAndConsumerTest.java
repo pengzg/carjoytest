ebcfd4ccdf8f4669c988d2dca79b9ab070cd4b9a
pengzg <pengzg@sqkx.net>
2021-08-12 11:27:25 +0800

    kaf

--- src/main/java/xyz/carjoy/kafka/kafka13/KafkaProducerTransactionsProducerAndConsumerTest.java
+++ src/main/java/xyz/carjoy/kafka/kafka13/KafkaProducerTransactionsProducerAndConsumerTest.java
@@ -51,6 +51,7 @@ public class KafkaProducerTransactionsProducerAndConsumerTest {
             }
         }
 
+        
         try {
             kafkaProducer.beginTransaction();
             for (int i = 0; i < 10; i++) {
